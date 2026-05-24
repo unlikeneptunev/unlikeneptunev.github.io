5fb004eff0229fe07fbd5e5dc2e854cb4c99af4d
unlikeneptunev <evellier@proton.me>
2026-05-24 16:17:57 +0700

    add github actions deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://unlikeneptune.github.io",
+  integrations: [mdx(), sitemap()],
+});
